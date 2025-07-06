01a2b7ac6ca82c7c0eacb076f1d3b7af589936e3
Behnaz MohammdHasani <18behnaz@gmail.com>
2025-07-06 17:53:56 +0330

    initial commit: add django bookstore project

---
+++ library/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from django.db import models
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
+    published_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
