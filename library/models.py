f2766e2d6e03502509f40f494b1f285ea9dbc57a
Behnaz MohammdHasani <18behnaz@gmail.com>
2025-07-12 22:17:50 +0330

    Edited genre model

--- library/models.py
+++ library/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db import models
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
