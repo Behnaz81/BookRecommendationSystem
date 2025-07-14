c16e1130012dad397e5290e9b6ce01480ed7bf7d
Behnaz MohammdHasani <18behnaz@gmail.com>
2025-07-14 22:37:58 +0330

    Author serializer was added to display authors' names

--- library/serializers.py
+++ library/serializers.py
@@ -1,8 +1,16 @@
 # serializers.py
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True)
+
     class Meta:
         model = Book
         fields = '__all__'
